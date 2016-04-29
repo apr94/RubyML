ae0eac46ef2bd93e6e1856297a884d3a80437054
Palimar Rao <palimar@seas.upenn.edu>
2016-04-29 08:35:41 -0400

    data methods tests

--- test/test_classifiermethods.rb
+++ test/test_classifiermethods.rb
@@ -4,4 +4,15 @@ require 'coveralls'
 Coveralls.wear!
 
 class ClassifierMethodsTest < Minitest::Test
+  def test_generate_folds
+    rml = RubyML.new
+    data = rml.load_data('test/data/testdata1', true)
+    x, y = rml.separate_data(data)
+    p = Perceptron.new(100, 3)
+    xtrain, ytrain, xtest, ytest = p.generate_folds(x, y, 1, 3)
+    assert_equal ytest, y['1', ':']
+    assert_equal xtest, x['1', ':']
+    assert_equal ytrain, y['0', ':'].vstack(y['2', ':'])
+    assert_equal xtrain, x['0', ':'].vstack(x['2', ':'])
+  end
 end
