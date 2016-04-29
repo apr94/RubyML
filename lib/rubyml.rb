d190e61b7bf89b7c55be6f5ca4d2aa37c40f6162
Palimar Rao <palimar@seas.upenn.edu>
2016-04-29 00:57:53 -0400

    slicing done

--- lib/rubyml.rb
+++ lib/rubyml.rb
@@ -23,7 +23,7 @@ class Matrix
   end
 
   def redirect2(i, j)
-    if i.include?(':') && j.include?(':')
+    if i.include?(':') || j.include?(':')
       redirect3(i, j)
     else
       old_element(i, j)
@@ -33,7 +33,28 @@ class Matrix
   def redirect3(i, j)
     rs, re = i.split(':').map { |e| e == '' ? 0 : Integer(e) }
     cs, ce = j.split(':').map { |e| e == '' ? 0 : Integer(e) }
+    redirect4(rs, re, cs, ce, [i, j])
+  end
+
+  def redirect4(rs, re, cs, ce, orig)
+    re = rs + 1 unless orig[0].include?(':')
+    ce = cs + 1 unless orig[1].include?(':')
+    redirect5(rs, re, cs, ce)
+  end
+
+  def redirect5(rs, re, cs, ce)
+    rs = rs.nil? ? 0 : rs
+    cs = cs.nil? ? 0 : cs
+    re = re.nil? ? row_count : re
+    ce = ce.nil? ? column_count : ce
+    redirect6(rs, re, cs, ce)
+  end
+
+  def redirect6(rs, re, cs, ce)
     return if rs >= re || cs >= ce
+    rv = row_vectors[rs..re - 1].map(&:to_a)
+    nrv = rv.map { |e| e[cs..ce - 1] }
+    Matrix.rows(nrv)
   end
 end
 
